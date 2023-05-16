b6047a1c339f85c580e5fa63dec1b519854227ed
Sean <scallen11@btinternet.com>
2023-05-16 20:06:52 +0100

    Need to revise events in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([
+    { id: 1, name: "Buy shopping" },
+    { id: 2, name: "Clean bathroom" },
+    { id: 3, name: "Car's MOT" },
+  ])
+
+  const [newItem, setNewItem] = useState("")
+
+  const taskComplete = (itemId) => {
+    // console.log("Purchase button clicked on:", itemId)
+    const newItems = items.filter((item) => item.id !== itemId)
+    setItems(newItems)
+  }
+
+  const listItems = items.map((item) => {
+    return (
+      <li key={item.id}>
+      {item.name}
+      <button onClick={() => taskComplete(item.id)}>Task Complete</button>
+      </li>
+    )
+  })
+
+  const handleItemInput = (event) => {
+    setNewItem(event.target.value)
+  }
+
+
+  const saveNewItem = (event)=>{
+    event.preventDefault();
+
+    const newItemObj = { id: Date.now(), name: newItem}
+    const nextItems = [... items, newItemObj]
+    setItems(nextItems)
+    setNewItem("")
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List - { items.length? "Keep Working" : "No Tasks to do!!" }</h1>
+      <hr></hr>
+      <form onSubmit={saveNewItem}>
+        <label htmlFor='new-item'>Add a new task:</label>
+        <input id='new-item' type='text' value={newItem} onChange={handleItemInput}/>
+        <input type='submit' value="Add New Task" />
+      </form>
+      <ul>
+      {listItems}
+      </ul>
+      
     </div>
   );
 }
